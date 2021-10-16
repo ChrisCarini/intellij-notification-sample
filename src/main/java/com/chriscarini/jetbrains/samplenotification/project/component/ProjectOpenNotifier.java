c03e11a4a0089c364d5ec2014e38a8774749c7b9
ChrisCarini <6374067+ChrisCarini@users.noreply.github.com>
2021-10-16 03:40:13 -0700

    Fixing a few problems identified by IntelliJ.

--- src/main/java/com/chriscarini/jetbrains/samplenotification/project/component/ProjectOpenNotifier.java
+++ src/main/java/com/chriscarini/jetbrains/samplenotification/project/component/ProjectOpenNotifier.java
@@ -10,7 +10,7 @@ import org.jetbrains.annotations.NotNull;
 public class ProjectOpenNotifier implements StartupActivity {
   @Override
   public void runActivity(@NotNull Project project) {
-    final Notification notification = new Notification("ProjectOpenNotification", "Project Opened Detected",
+    final Notification notification = new Notification("ProjectOpenNotification", "Project opened detected",
         String.format("You just opened %s", project.getName()), NotificationType.INFORMATION);
     notification.notify(project);
   }
