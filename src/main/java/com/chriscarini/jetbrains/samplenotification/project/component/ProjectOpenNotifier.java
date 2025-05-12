b7d3a2b6df1e8934a020dff78c7eb258fc1ca805
Chris Carini <6374067+ChrisCarini@users.noreply.github.com>
2025-05-12 00:13:14 -0700

    Upgrading IntelliJ from 2025.1 to 2025.1.1 (#474)
    * Upgrading IntelliJ from 2025.1 to 2025.1.1
    
    * Migrate from `StartupActivity` (obsolete) -> `ProjectActivity`
    
    Fixes #475

--- src/main/java/com/chriscarini/jetbrains/samplenotification/project/component/ProjectOpenNotifier.java
+++ src/main/java/com/chriscarini/jetbrains/samplenotification/project/component/ProjectOpenNotifier.java
@@ -2,16 +2,24 @@ package com.chriscarini.jetbrains.samplenotification.project.component;
 
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
+import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.openapi.startup.ProjectActivity;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 
-public class ProjectOpenNotifier implements StartupActivity {
+public class ProjectOpenNotifier implements ProjectActivity {
+
   @Override
-  public void runActivity(@NotNull Project project) {
-    final Notification notification = new Notification("ProjectOpenNotification", "Project opened detected",
-        String.format("You just opened %s", project.getName()), NotificationType.INFORMATION);
-    notification.notify(project);
+  public @Nullable Object execute(@NotNull Project project, @NotNull Continuation<? super Unit> continuation) {
+    DumbService.getInstance(project).runWhenSmart(() -> {
+      final Notification notification = new Notification("ProjectOpenNotification", "Project opened detected",
+          String.format("You just opened %s", project.getName()), NotificationType.INFORMATION);
+      notification.notify(project);
+    });
+    return null;
   }
 }
